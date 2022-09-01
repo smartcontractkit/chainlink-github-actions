84419f9a2c3ca4cc5410658ad47060ea684045d5
Tate <tate.exon@smartcontract.com>
2022-09-01 15:26:20 -0600

    add args and move to using non global actions

--- public-to-private/poll-workflow/main.go
+++ public-to-private/poll-workflow/main.go
@@ -26,10 +26,10 @@ type Inputs struct {
 }
 
 // getInputs Loads inputs from the environment
-func getInputs() *Inputs {
+func getInputs(c *Common) *Inputs {
 	var inputs Inputs
 	if err := envconfig.Process("", &inputs); err != nil {
-		githubactions.Fatalf("could not load inputs from environment: %v", err)
+		c.action.Fatalf("could not load inputs from environment: %v", err)
 	}
 
 	return &inputs
@@ -41,7 +41,7 @@ type Outputs struct {
 }
 
 // setOutputs Sets the outputs in the format that a docker action can parse
-func (o *Outputs) setOutputs() {
+func (o *Outputs) setOutputs(c *Common) {
 	ao := *o
 	val := reflect.ValueOf(ao)
 	typeOfS := val.Type()
@@ -49,8 +49,8 @@ func (o *Outputs) setOutputs() {
 	for i := 0; i < val.NumField(); i++ {
 		k := strings.ToLower(typeOfS.Field(i).Name)
 		v := fmt.Sprintf("%v", val.Field(i).Interface())
-		githubactions.Infof("Setting output: %s = %s", k, v)
-		githubactions.SetOutput(k, v)
+		c.action.Infof("Setting output: %s = %s", k, v)
+		c.action.SetOutput(k, v)
 	}
 }
 
@@ -58,6 +58,7 @@ type Common struct {
 	ctx        context.Context
 	client     *github.Client
 	workflowID int64
+	action     *githubactions.Action
 }
 
 // connectToGithub Sets up the github client and connects
@@ -87,17 +88,17 @@ func (c *Common) getMostRecentWorkflowRunId(inputs *Inputs) {
 
 		runs, resp, err = c.client.Actions.ListWorkflowRunsByFileName(c.ctx, inputs.Owner, inputs.Repository, inputs.WorkflowFile, opts)
 		if err != nil {
-			githubactions.Infof("failed to get the workflow: %v", err)
+			c.action.Infof("failed to get the workflow: %v", err)
 			continue
 		}
 		if resp.StatusCode != 200 {
 			err = fmt.Errorf("failed to get the workflow, status code %d", resp.StatusCode)
-			githubactions.Infof("%v", err)
+			c.action.Infof("%v", err)
 			continue
 		}
 		if runs.GetTotalCount() < 1 {
 			err = fmt.Errorf("failed fo find any workflow runs")
-			githubactions.Infof("%v", err)
+			c.action.Infof("%v", err)
 			continue
 		}
 
@@ -105,7 +106,7 @@ func (c *Common) getMostRecentWorkflowRunId(inputs *Inputs) {
 		var currentWorkflow *github.WorkflowRun
 		var timestamp *github.Timestamp
 		for i, wf := range runs.WorkflowRuns {
-			githubactions.Infof("workflow at index %d had completion status of %s at %s with id %d", i, *wf.Status, wf.CreatedAt.Time.String(), *wf.ID)
+			c.action.Infof("workflow at index %d had completion status of %s at %s with id %d", i, *wf.Status, wf.CreatedAt.Time.String(), *wf.ID)
 			if timestamp == nil {
 				timestamp = wf.CreatedAt
 			}
@@ -114,17 +115,17 @@ func (c *Common) getMostRecentWorkflowRunId(inputs *Inputs) {
 				if currentWorkflow == nil {
 					currentWorkflow = wf
 				}
-				githubactions.Infof("found an active workflow")
+				c.action.Infof("found an active workflow")
 				// we want the latest workflow so if this one is newer, use it
 				if wf.CreatedAt.After(timestamp.Time) {
-					githubactions.Infof("workflow is newer, using it unless we find a newer one")
+					c.action.Infof("workflow is newer, using it unless we find a newer one")
 					currentWorkflow = wf
 				}
 			}
 		}
 		if currentWorkflow == nil {
 			err = fmt.Errorf("failed to find an unfinished workflow")
-			githubactions.Infof("%v", err)
+			c.action.Infof("%v", err)
 			continue
 		}
 		workflow = currentWorkflow
@@ -132,7 +133,7 @@ func (c *Common) getMostRecentWorkflowRunId(inputs *Inputs) {
 	}
 	// fail if we ended the retry loop in error
 	if err != nil {
-		githubactions.Fatalf("did not find any active workflows: %v", err)
+		c.action.Fatalf("did not find any active workflows: %v", err)
 	}
 
 	c.workflowID = *workflow.ID
@@ -142,14 +143,14 @@ func (c *Common) getMostRecentWorkflowRunId(inputs *Inputs) {
 func (c *Common) getWorkflowRun(inputs *Inputs) *github.WorkflowRun {
 	wfr, resp, err := c.client.Actions.GetWorkflowRunByID(c.ctx, inputs.Owner, inputs.Repository, c.workflowID)
 	if err != nil {
-		githubactions.Infof("Failed to get the workflow run: %v", err)
+		c.action.Infof("Failed to get the workflow run: %v", err)
 		return nil
 	}
 	if resp.StatusCode != 200 {
-		githubactions.Infof("Failed to get the workflow, found status code: %d", resp.StatusCode)
+		c.action.Infof("Failed to get the workflow, found status code: %d", resp.StatusCode)
 		return nil
 	}
-	githubactions.Infof("current workflow run status: %v", *wfr.Status)
+	c.action.Infof("current workflow run status: %v", *wfr.Status)
 	return wfr
 }
 
@@ -182,7 +183,7 @@ func (c *Common) pollWorkflow(inputs *Inputs) *Outputs {
 	}
 
 	if status != COMPLETED_STATUS {
-		githubactions.Fatalf("workflow did not reach completed status, id: %d", c.workflowID)
+		c.action.Fatalf("workflow did not reach completed status, id: %d", c.workflowID)
 	}
 
 	return &Outputs{
@@ -192,15 +193,17 @@ func (c *Common) pollWorkflow(inputs *Inputs) *Outputs {
 }
 
 func main() {
-	inputs := getInputs()
 
 	common := &Common{
-		ctx: context.Background(),
+		ctx:    context.Background(),
+		action: githubactions.New(),
 	}
+
+	inputs := getInputs(common)
 
 	common.connectToGithub(inputs)
 	common.getMostRecentWorkflowRunId(inputs)
 
 	outputs := common.pollWorkflow(inputs)
-	outputs.setOutputs()
+	outputs.setOutputs(common)
 }
