0269edd9e1c7b747376401206d80f74e03c446b1
chainchad <96362174+chainchad@users.noreply.github.com>
2023-06-14 10:48:20 -0400

    Add unit tests

---
+++ semver-compare/__tests__/main.test.ts
@@ -0,0 +1,37 @@
+import {versionCompare} from '../src/main'
+import semver from 'semver'
+
+describe('versionCompare', () => {
+  test('should throw an error when no versions or operator specified', () => {
+    expect(() => versionCompare('', 'gt', '')).toThrow(
+      'Required inputs not specified.'
+    )
+    expect(() => versionCompare('1.0.0', 'gt', '')).toThrow(
+      'Required inputs not specified.'
+    )
+    expect(() => versionCompare('', 'gt', '1.0.0')).toThrow(
+      'Required inputs not specified.'
+    )
+  })
+
+  test('should throw an error when versions are invalid', () => {
+    expect(() => versionCompare('abc', 'gt', '1.0.0')).toThrow(
+      'Invalid version(s).'
+    )
+    expect(() => versionCompare('1.0.0', 'gt', 'abc')).toThrow(
+      'Invalid version(s).'
+    )
+  })
+
+  test('should throw an error when operator is invalid', () => {
+    expect(() => versionCompare('1.0.0', 'abc' as any, '1.0.0')).toThrow(
+      'Invalid operator.'
+    )
+  })
+
+  test('should return the correct result', () => {
+    expect(versionCompare('1.0.0', 'gt', '0.1.0')).toBe(true)
+    expect(versionCompare('1.0.0', 'lt', '2.0.0')).toBe(true)
+    expect(versionCompare('1.0.0', 'eq', '1.0.0')).toBe(true)
+  })
+})
--- semver-compare/src/main.ts
+++ semver-compare/src/main.ts
@@ -1,42 +1,48 @@
-import * as core from '@actions/core';
-import semver from 'semver';
+import * as core from '@actions/core'
+import semver from 'semver'
 
 /**
  * Simple GitHub Actions wrapper around the semver library.
  */
 
-async function run(): Promise<void> {
-      try { 
-        const version1 = core.getInput('version1');
-        const operator = core.getInput('operator');
-        const version2 = core.getInput('version2');
-        core.debug(`Comparing ${version1} ${operator} ${version2}`);
+type Operator = 'gt' | 'lt' | 'eq'
 
-        // Define valid operators
-        type Operator = 'gt' | 'lt' | 'eq';
-
-        // Check for required input:
-        if (!version1 || !version2 || !operator) {
-          throw new Error('Required inputs not specified.');
-        }
-
-        // Validate the inputs:
-        if (!semver.valid(version1) || !semver.valid(version2)) {
-          throw new Error('Invalid version(s).');
-        }
-
-        if (!['gt', 'lt', 'eq'].includes(operator as Operator)) {
-          throw new Error('Invalid operator.');
-        }
-
-        // Compare the versions:
-        const result = semver[operator as Operator](version1, version2);
-        core.setOutput('result', result);
-    } 
-    catch (error) {
-      if (error instanceof Error) core.setFailed(error.message);
-    }
+export function versionCompare(
+  version1: string,
+  operator: Operator,
+  version2: string
+): boolean {
+  // Check for required input:
+  if (!version1 || !version2 || !operator) {
+    throw new Error('Required inputs not specified.')
   }
 
-  run()
-  
+  // Validate the inputs:
+  if (!semver.valid(version1) || !semver.valid(version2)) {
+    throw new Error('Invalid version(s).')
+  }
+
+  if (!['gt', 'lt', 'eq'].includes(operator)) {
+    throw new Error('Invalid operator.')
+  }
+
+  // Compare the versions:
+  const result = semver[operator](version1, version2)
+  return result
+}
+
+async function run(): Promise<void> {
+  try {
+    const version1 = core.getInput('version1')
+    const operator = core.getInput('operator') as Operator
+    const version2 = core.getInput('version2')
+    core.debug(`Comparing ${version1} ${operator} ${version2}`)
+
+    const result = versionCompare(version1, operator, version2)
+    core.setOutput('result', result)
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run()
