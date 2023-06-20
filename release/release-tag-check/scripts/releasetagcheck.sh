d5ab9fe53da374dd789b5d9ef10f3282197e088d
chainchad <96362174+chainchad@users.noreply.github.com>
2023-06-20 13:39:19 -0400

    Allow overriding pre-release and release regex string (#98)
    Allow overriding prerelease and release regex string

--- release/release-tag-check/scripts/releasetagcheck.sh
+++ release/release-tag-check/scripts/releasetagcheck.sh
@@ -8,7 +8,15 @@ set -euo pipefail
 # Examples:
 #   1. v1.2.3-beta.0 -> pre-release
 #   2. v1.2.3 -> release
+#
+# Override default regex by setting these env vars:
+#   - RELEASE_REGEX
+#   - PRE_RELEASE_REGEX
 ##
+
+# Configurable regex patterns with defaults
+RELEASE_REGEX=${RELEASE_REGEX:-"^v[0-9]+\.[0-9]+\.[0-9]+$"}
+PRE_RELEASE_REGEX=${PRE_RELEASE_REGEX:-"^v[0-9]+\.[0-9]+\.[0-9]+-(.+)$"}
 
 if [[ -z "${GITHUB_REF:-}" ]]; then
     echo "ERROR: GITHUB_REF environment variable is required"
@@ -21,9 +29,9 @@ echo "The tag name is $TAG_NAME".
 echo "Checking if $TAG_NAME is a release or pre-release tag..."
 IS_RELEASE=false
 IS_PRE_RELEASE=false
-if [[ $TAG_NAME =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+if [[ $TAG_NAME =~ $RELEASE_REGEX ]]; then
     IS_RELEASE="true"
-elif [[ $TAG_NAME =~ ^v[0-9]+\.[0-9]+\.[0-9]+-(.+)$ ]]; then
+elif [[ $TAG_NAME =~ $PRE_RELEASE_REGEX ]]; then
     IS_PRE_RELEASE="true"
 fi
 echo "is-release=${IS_RELEASE}" | tee -a "$GITHUB_OUTPUT"
