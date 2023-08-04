b351a8cc26894a3dd932a176a5ecbfa3d270f0d3
Tate <tate.exon@smartcontract.com>
2023-08-04 09:26:13 -0600

    Update compiled js files

--- go/go-test-results-parsing/dist/index.js
+++ go/go-test-results-parsing/dist/index.js
@@ -6723,12 +6723,13 @@ function getTestFailures(testResults) {
                 names.push(testResult.Test);
             }
             else {
-                // we got a failure without a test failure
+                // it is possible to have a package failure without a test failure
+                // so we are storing whether we have a package failure to determine
+                // if we need to output all the output for triage later for panic cases
                 packageFailure = true;
             }
         }
     });
-    // const out: TestRunFailures = {TestsFailed: names, PackageFailure: packageFailure}
     return { TestsFailed: names, PackageFailure: packageFailure };
 }
 exports.getTestFailures = getTestFailures;
