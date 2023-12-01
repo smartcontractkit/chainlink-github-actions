c4e9a27b1eebd707299876105fbab5ccdd4a7e5c
Tate <tate.exon@smartcontract.com>
2023-12-01 15:39:26 -0700

    [TT-744] Update to stream failure data and handle panics better

--- go/go-test-results-parsing/__tests__/main.test.ts
+++ go/go-test-results-parsing/__tests__/main.test.ts
@@ -1,61 +1,123 @@
-import {
-  deserializeTestResultsFile,
-  jsonTestOutput,
-  getTestFailures,
-  standardTestOutput,
-  logAllOutput
-} from '../src/main'
-import {TestResult, TestRunFailures} from '../src/types'
-const file = './__tests__/fixtures/go_test_results_input.json'
+import * as fs from 'fs'
+import * as path from 'path'
+import {preprocessTestResultsFile, processTestResultsFile} from '../src/main'
+import {PassFailMapItem} from '../src/types'
+
+const fileBase = './__tests__/fixtures/'
+const tmpFile = `${fileBase}tmp_test_`
+
+function cleanUpTestFiles(): void {
+  const files = fs.readdirSync(fileBase)
+  files.forEach(file => {
+    if (file.startsWith('tmp_test_')) {
+      const filePath = path.join(fileBase, file)
+
+      try {
+        fs.unlinkSync(filePath)
+      } catch (unlinkErr) {
+        console.error('Error deleting file:', unlinkErr)
+      }
+    }
+  })
+}
+
 describe('output testing', () => {
-  test('can deserialize file', () => {
-    const testResults: TestResult[] = deserializeTestResultsFile(file)
-    expect(testResults).toMatchSnapshot()
-    expect(testResults.length).toBe(11)
+  // cleanup test files before and after test
+  beforeAll(() => {
+    cleanUpTestFiles()
   })
-  test('failed test list', () => {
-    const testResults: TestResult[] = deserializeTestResultsFile(file)
-    const failedTests: TestRunFailures = getTestFailures(testResults)
-    expect(failedTests.TestsFailed).toMatchSnapshot()
-    expect(failedTests.TestsFailed.length).toBe(1)
-    expect(failedTests.PackageFailure).toBe(true)
+  afterAll(() => {
+    cleanUpTestFiles()
   })
-  test('standard test output', () => {
-    const testResults: TestResult[] = deserializeTestResultsFile(file)
-    const failedTests: TestRunFailures = getTestFailures(testResults)
-    const filteredResults: TestResult[] = standardTestOutput(
-      failedTests.TestsFailed,
-      testResults,
-      true
+
+  it('can read file with all passes', async () => {
+    const outputFile = `${tmpFile}all_passing.txt`
+    const inputFile = `${fileBase}all_passing.json`
+    const failureMap: Map<string, PassFailMapItem> =
+      await preprocessTestResultsFile(inputFile)
+    const pass: boolean = await processTestResultsFile(
+      failureMap,
+      inputFile,
+      false,
+      outputFile
     )
-    expect(filteredResults).toMatchSnapshot()
-    expect(filteredResults.length).toBe(1)
-    expect(failedTests.PackageFailure).toBe(true)
+    expect(pass).toBe(true)
+    expect(fs.existsSync(outputFile)).toBe(false)
   })
-  test('json test output', () => {
-    const testResults: TestResult[] = deserializeTestResultsFile(file)
-    const failedTests: TestRunFailures = getTestFailures(testResults)
-    const filteredResults: TestResult[] = jsonTestOutput(
-      failedTests.TestsFailed,
-      testResults,
-      true
+  it('can read file with all failures', async () => {
+    const outputFile = `${tmpFile}all_failing.txt`
+    const inputFile = `${fileBase}all_failing.json`
+    const failureMap: Map<string, PassFailMapItem> =
+      await preprocessTestResultsFile(inputFile)
+    const pass: boolean = await processTestResultsFile(
+      failureMap,
+      inputFile,
+      false,
+      outputFile
     )
-    expect(filteredResults).toMatchSnapshot()
-    expect(filteredResults.length).toBe(5)
-    expect(failedTests.PackageFailure).toBe(true)
+    expect(pass).toBe(false)
+    const fileContent = fs.readFileSync(outputFile, 'utf-8')
+    expect(fileContent).toMatchSnapshot()
   })
-  test('failure without a test failure', () => {
-    const file = './__tests__/fixtures/failure_output_without_test_failure.json'
-    const testResults: TestResult[] = deserializeTestResultsFile(file)
-    const failedTests: TestRunFailures = getTestFailures(testResults)
-    const filteredResults: TestResult[] = standardTestOutput(
-      failedTests.TestsFailed,
-      testResults,
-      true
+  it('can read file with mix of pass and failures', async () => {
+    const outputFile = `${tmpFile}pass_fail_mix.txt`
+    const inputFile = `${fileBase}pass_fail_mix.json`
+    const failureMap: Map<string, PassFailMapItem> =
+      await preprocessTestResultsFile(inputFile)
+    const pass: boolean = await processTestResultsFile(
+      failureMap,
+      inputFile,
+      false,
+      outputFile
     )
-    expect(filteredResults).toMatchSnapshot()
-    expect(filteredResults.length).toBe(0)
-    expect(failedTests.PackageFailure).toBe(true)
-    logAllOutput(testResults)
+    expect(pass).toBe(false)
+    const fileContent = fs.readFileSync(outputFile, 'utf-8')
+    expect(fileContent).toMatchSnapshot()
+  })
+  it('can read file with mix of pass and failures failures and non json txt injected from other potential errors in the go runner', async () => {
+    const outputFile = `${tmpFile}pass_fail_mix_with_non_json.txt`
+    const inputFile = `${fileBase}pass_fail_mix_with_non_json.json`
+    const failureMap: Map<string, PassFailMapItem> =
+      await preprocessTestResultsFile(inputFile)
+    const pass: boolean = await processTestResultsFile(
+      failureMap,
+      inputFile,
+      false,
+      outputFile
+    )
+    expect(pass).toBe(false)
+    const fileContent = fs.readFileSync(outputFile, 'utf-8')
+    expect(fileContent).toMatchSnapshot()
+  })
+
+  it('can parse a panic for a test name and only output logs for that panic from that package', async () => {
+    const outputFile = `${tmpFile}package_panic_with_test_name.txt`
+    const inputFile = `${fileBase}package_panic_with_test_name.json`
+    const failureMap: Map<string, PassFailMapItem> =
+      await preprocessTestResultsFile(inputFile)
+    const pass: boolean = await processTestResultsFile(
+      failureMap,
+      inputFile,
+      false,
+      outputFile
+    )
+    expect(pass).toBe(false)
+    const fileContent = fs.readFileSync(outputFile, 'utf-8')
+    expect(fileContent).toMatchSnapshot()
+  })
+  it('can read file with package failure but no test failure', async () => {
+    const outputFile = `${tmpFile}failing_package_without_test_fail.txt`
+    const inputFile = `${fileBase}failing_package_without_test_fail.json`
+    const failureMap: Map<string, PassFailMapItem> =
+      await preprocessTestResultsFile(inputFile)
+    const pass: boolean = await processTestResultsFile(
+      failureMap,
+      inputFile,
+      false,
+      outputFile
+    )
+    expect(pass).toBe(false)
+    const fileContent = fs.readFileSync(outputFile, 'utf-8')
+    expect(fileContent).toMatchSnapshot()
   })
 })
